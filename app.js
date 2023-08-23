f70d2e3ae868fd420fe8566ec3867ba79f389f36
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-08-23 09:21:03 -0300

    criando Middleware

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const rotaLivro = require("./rotas/livro");
+const asyncErrors = require("express-async-errors");
 const app = express();
 app.use(express.json());
 
 app.use("/livros", rotaLivro);
+const errorMiddleware = require("./helpers/errorMiddleware");
+app.use(errorMiddleware);
+app.use((err, req, res, next) => {
+	console.error(err); // Registrar o erro para fins de depuração
 
+	// Tratar o erro e enviar uma resposta adequada ao cliente
+	res.status(500).json({ error: "Ocorreu um erro interno." });
+});
 const port = 8000;
 
 app.listen(port, () => {
--- contraladores/livro.js
+++ contraladores/livro.js
@@ -1,3 +1,4 @@
+const asyncErrors = require("express-async-errors");
 const {
 	getTodosLivros,
 	getLivroPorId,
@@ -5,58 +6,36 @@ const {
 	modificaLivro,
 	deletaLivroPorId,
 } = require("../servicos/livro");
+
 async function getLivros(req, res) {
-	try {
-		const livros = await getTodosLivros();
-		res.send(livros);
-	} catch (error) {
-		res.status(500);
-		res.send(error.message);
-	}
+	const livros = await getTodosLivros();
+	res.send(livros);
 }
+
 async function getLivro(req, res) {
-	try {
-		const id = req.params.id;
-		const livro = await getLivroPorId(id);
-		res.send(livro);
-	} catch (error) {
-		res.status(500);
-		res.send(error.message);
-	}
+	const id = req.params.id;
+	const livro = await getLivroPorId(id);
+	res.send(livro);
 }
 
 async function postLivro(req, res) {
-	try {
-		const livroNovo = req.body;
-		await insereLivro(livroNovo);
-		res.status(201);
-		res.send("livro novo inserido com sucesso");
-	} catch (error) {
-		res.status(500);
-		res.send(error.message);
-	}
+	const livroNovo = req.body;
+	await insereLivro(livroNovo);
+	res.status(201).send("livro novo inserido com sucesso");
 }
-async function patchLivro(req, res) {
-	try {
-		const id = req.params.id;
-		const body = req.body;
 
-		await modificaLivro(body, id);
-		res.send("item modificado com sucesso");
-	} catch (error) {
-		res.status(500);
-		res.send(error.message);
-	}
+async function patchLivro(req, res) {
+	const id = req.params.id;
+	const body = req.body;
+
+	await modificaLivro(body, id);
+	res.send("item modificado com sucesso");
 }
+
 async function deleteLivro(req, res) {
-	try {
-		const id = req.params.id;
-		await deletaLivroPorId(id);
-		res.send("item deletado com sucesso");
-	} catch (error) {
-		res.status(500);
-		res.send(error.message);
-	}
+	const id = req.params.id;
+	await deletaLivroPorId(id);
+	res.send("item deletado com sucesso");
 }
 
 module.exports = {
