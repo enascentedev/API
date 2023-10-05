0431cc320ddd4ebe493ce8e2c1302d7a25f80d33
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-10-04 23:43:11 -0300

    instalando jsonwebtoken, bcrypt,mongoose

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose"); // Novo
 const rotaLivro = require("./rotas/livro");
 const asyncErrors = require("express-async-errors");
+
+// Conexão com MongoDB - Novo
+mongoose
+	.connect("mongodb://localhost:27017/livrosAPI", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("MongoDB connected"))
+	.catch((err) => console.log(err));
+
 const app = express();
 app.use(express.json());
 
 app.use("/livros", rotaLivro);
+
 const errorMiddleware = require("./helpers/errorMiddleware");
 app.use(errorMiddleware);
+
 app.use((err, req, res, next) => {
 	console.error(err); // Registrar o erro para fins de depuração
-
-	// Tratar o erro e enviar uma resposta adequada ao cliente
 	res.status(500).json({ error: "Ocorreu um erro interno." });
 });
+
 const port = 8000;
 
 app.listen(port, () => {
